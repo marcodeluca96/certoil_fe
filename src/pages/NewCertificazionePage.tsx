5d292fc4656f25ed3f48c9ec169e04d88f4cc2fb
marco.deluca <marco.deluca@weavesrl.com>
2026-02-17 09:49:59 +0100

    feat: Introduce new `Field`, `Input`, `Label`, `Switch`, and `Separator` UI components and create `NewCertificazionePage`.

--- src/pages/NewCertificazionePage.tsx
+++ src/pages/NewCertificazionePage.tsx
@@ -1,8 +1,26 @@
+import { FieldGroup, FieldSet, Field, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
 
 const NewCertificazionePage = () => {
     return (
-        <div>
-            NewCertificazionePage
+        <div className="flex flex-row gap-4 bg-white p-4 rounded-lg shadow-lg w-auto m-4">
+            <FieldSet className="flex-1">
+                <FieldGroup>
+                    <Field>
+                        <FieldLabel htmlFor="name">Full name</FieldLabel>
+                        <Input id="name" autoComplete="off" placeholder="Evil Rabbit" />
+                    </Field>
+                </FieldGroup>
+            </FieldSet>
+            <FieldSet className="flex-1">
+                <FieldGroup>
+                    <Field>
+                        <FieldLabel htmlFor="name">Full name</FieldLabel>
+                        <Input id="name" autoComplete="off" placeholder="Evil Rabbit" />
+                    </Field>
+                </FieldGroup>
+            </FieldSet>
         </div>
     );
 };
